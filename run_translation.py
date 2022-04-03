64c2d980f4cfde33e06a0fa243bcbc7140669db2
lynnettetee <43146875+lynnettetee@users.noreply.github.com>
2022-04-03 02:36:23 -0400

    Clean run.py
    Add parser to accept the command line option '-p': ie to plot networkx graph or not

--- run_translation.py
+++ run_translation.py
@@ -1,14 +1,20 @@
-# TODO need main function?
-import networkx as nx # TODO rb already imported this - how to avoid importing it again here? move all nx calls to within the rb function?
+import networkx as nx 
 import matplotlib.pyplot as plt
 import pandas as pd
 import nltk
 from transformers import BertTokenizer, BertModel
 from nltk.corpus import stopwords
+import argparse
 
 from rule_based.main import RuleBased
 from clustering.main import Clustering 
 from wordnet.main import WordNet
+
+# Command line options 
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--plot', action='store_true', default=False, help='if True, generates NetworkX graphs.')
+args = parser.parse_args()
+plot_graph = bool(args.plot)
 
 file_path = 'files/'
 
@@ -35,6 +41,7 @@ rb_tagger = rb_tagger.tag
 
 rb = RuleBased(rb_grammar, rb_chunker, rb_lemmatizer, rb_tagger)
 
+
 # Initialize Clustering model
 df = pd.read_csv(file_path + "iris.csv")
 ## Load pre-trained model (weights)
@@ -45,55 +52,49 @@ model = BertModel.from_pretrained('bert-base-uncased',
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 clustering = Clustering(model, tokenizer, rb_lemmatizer, df)
 
+
 # Initialize WordNet model
-wn = WordNet()
-
-### translation output
-matched_indicators = pd.read_csv(file_path + 'glossarymatchedindicators.csv', index_col=0, header=None, squeeze=True).to_dict()
-cids_classes = pd.read_csv(file_path + 'cidsclasses.csv', header=None, squeeze=True).to_list()
-t1_text = 'Number of individuals residing in rural areas who sold goods or services to the organization during the reporting period.'
-pos_breakdown = rb.get_key_pos_tag(t1_text)
-ind_code = 'PI2566'
-pos_breakdown_output = rb.rb_translation(pos_breakdown, ind_code, cids_classes, matched_indicators)
-
-# networkx
-G, prop_rel = rb.plot_KG(pos_breakdown_output)
-pos = nx.spring_layout(G, k=0.2)
-plt.figure(figsize=(15,8))                                                    
-nx.draw(G, pos, with_labels=True, arrows=True)                                                              
-nx.draw_networkx_edge_labels(G,pos,edge_labels=nx.get_edge_attributes(G,'label'))
-
-# plt.show() # TODO 
+wn = WordNet(similarity_threshold=0.8, 
+			t_ss_method='lesk',
+			c_ss_method='lesk',
+			last_subword_weight=0.6)
 
 
-# ===== Demo =====
+# Run test set 
 indtestset = pd.read_csv(file_path + 'indicatortestset.csv', encoding = "ISO-8859-1", engine='python')
+cids_classes = pd.read_csv(file_path + 'cidsclasses.csv', header=None, squeeze=True).to_list()
+matched_indicators = pd.read_csv(file_path + 'glossarymatchedindicators.csv', index_col=0, header=None, squeeze=True).to_dict()
 
+# Peform translation on each sentence in indicatortestset.csv
 for index, row in indtestset.iterrows():
 	ind_code = row['Indicator Code']
 	ind_def = row['Indicator Definition']
 	print(ind_code)
 	print(ind_def)
-	#output_kg(ind_code, ind_def)
-
+	
+	# Step 1: POS tag sentence
 	pos_tagged = rb.get_key_pos_tag(ind_def)
 	rb_output = rb.rb_translation(pos_tagged, ind_code, cids_classes, matched_indicators)
 	print('rb_output')
 	print(rb_output)
-	clusterMap, kmeans = clustering.cluster()
-	cluster_output = clustering.rb_cluster_combined(rb_output, ind_def, clusterMap, kmeans)
-	print('cluster_output')
-	print(cluster_output)
+
+	# clusterMap, kmeans = clustering.cluster()
+	# cluster_output = clustering.rb_cluster_combined(rb_output, ind_def, clusterMap, kmeans)
+	# print('cluster_output')
+	# print(cluster_output)
+
+	# wordnet_output = wn.wordnet(ind_def, cluster_output)
 	wordnet_output = wn.wordnet(ind_def, rb_output)
 	print('wordnet_output')
 	print(wordnet_output)
-	if cluster_output != wordnet_output: import pdb;pdb.set_trace() # TODO rm
 
-	G, prop_rel = rb.plot_KG(cluster_output)
+	# Plot networkx graph
+	G, prop_rel = rb.plot_KG(wordnet_output)
 	pos = nx.spring_layout(G, k=0.2)
 	plt.figure(figsize=(15,8))                                                    
 	nx.draw(G, pos, with_labels=True, arrows=True)                                                              
 	nx.draw_networkx_edge_labels(G,pos,edge_labels=nx.get_edge_attributes(G,'label'))
 
-	#   plt.show()
+	if plot_graph:
+		plt.show() 
 	print('\n')
